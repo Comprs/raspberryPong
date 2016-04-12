69fe324d1c6b9179d1c6d596f7e2df6a8bc018e7
Peter Ryan Smith <comprs@outlook.com>
2016-04-12 21:41:24 +0100

    Update the Game of Life example to use colours

--- gol.py
+++ gol.py
@@ -4,6 +4,7 @@
 # capabilities of the terminal_writer
 
 from time import sleep
+import terminal_writer
 from terminal_writer import Writer
 from serial import Serial
 
@@ -17,7 +18,7 @@ class World:
         self.writer = Writer(output_file = serial_port)
 
     def update(self):
-        new_cells = set()
+        new_cells = {}
         for x in range(WORLD_WIDTH):
             for y in range(WORLD_HEIGHT):
                 neighbor_count = (int((x - 1, y - 1) in self.alive_cells) +
@@ -29,9 +30,9 @@ class World:
                                   int((x    , y + 1) in self.alive_cells) +
                                   int((x + 1, y + 1) in self.alive_cells))
                 if neighbor_count == 3:
-                    new_cells.add((x, y))
+                    new_cells[(x, y)] = terminal_writer.COLOUR_BLUE
                 elif neighbor_count == 2 and (x, y) in self.alive_cells:
-                    new_cells.add((x, y))
+                    new_cells[(x, y)] = terminal_writer.COLOUR_CYAN
         self.alive_cells = new_cells
 
     def render(self):
@@ -46,42 +47,42 @@ class World:
 if __name__ == "__main__":
     try:
         world = World({
-            (0, 24),
-            (1, 22),
-            (1, 24),
-            (2, 12),
-            (2, 13),
-            (2, 20),
-            (2, 21),
-            (2, 34),
-            (2, 35),
-            (3, 11),
-            (3, 15),
-            (3, 20),
-            (3, 21),
-            (3, 34),
-            (3, 35),
-            (4, 0),
-            (4, 1),
-            (4, 10),
-            (4, 16),
-            (4, 20),
-            (4, 21),
-            (5, 0),
-            (5, 1),
-            (5, 10),
-            (5, 14),
-            (5, 16),
-            (5, 17),
-            (5, 22),
-            (5, 24),
-            (6, 10),
-            (6, 16),
-            (6, 24),
-            (7, 11),
-            (7, 15),
-            (8, 12),
-            (8, 13),
+            (0, 24): terminal_writer.COLOUR_RED,
+            (1, 22): terminal_writer.COLOUR_RED,
+            (1, 24): terminal_writer.COLOUR_RED,
+            (2, 12): terminal_writer.COLOUR_RED,
+            (2, 13): terminal_writer.COLOUR_RED,
+            (2, 20): terminal_writer.COLOUR_RED,
+            (2, 21): terminal_writer.COLOUR_RED,
+            (2, 34): terminal_writer.COLOUR_RED,
+            (2, 35): terminal_writer.COLOUR_RED,
+            (3, 11): terminal_writer.COLOUR_RED,
+            (3, 15): terminal_writer.COLOUR_RED,
+            (3, 20): terminal_writer.COLOUR_RED,
+            (3, 21): terminal_writer.COLOUR_RED,
+            (3, 34): terminal_writer.COLOUR_RED,
+            (3, 35): terminal_writer.COLOUR_RED,
+            (4, 0): terminal_writer.COLOUR_RED,
+            (4, 1): terminal_writer.COLOUR_RED,
+            (4, 10): terminal_writer.COLOUR_RED,
+            (4, 16): terminal_writer.COLOUR_RED,
+            (4, 20): terminal_writer.COLOUR_RED,
+            (4, 21): terminal_writer.COLOUR_RED,
+            (5, 0): terminal_writer.COLOUR_RED,
+            (5, 1): terminal_writer.COLOUR_RED,
+            (5, 10): terminal_writer.COLOUR_RED,
+            (5, 14): terminal_writer.COLOUR_RED,
+            (5, 16): terminal_writer.COLOUR_RED,
+            (5, 17): terminal_writer.COLOUR_RED,
+            (5, 22): terminal_writer.COLOUR_RED,
+            (5, 24): terminal_writer.COLOUR_RED,
+            (6, 10): terminal_writer.COLOUR_RED,
+            (6, 16): terminal_writer.COLOUR_RED,
+            (6, 24): terminal_writer.COLOUR_RED,
+            (7, 11): terminal_writer.COLOUR_RED,
+            (7, 15): terminal_writer.COLOUR_RED,
+            (8, 12): terminal_writer.COLOUR_RED,
+            (8, 13): terminal_writer.COLOUR_RED,
         })
         world.start_loop()
     except KeyboardInterrupt:
