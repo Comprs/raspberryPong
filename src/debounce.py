28cd97ae1a93df68b3c636b7d583e7d49df17b3c
Peter Ryan Smith <ps1011@york.ac.uk>
2016-05-02 14:49:23 +0100

    Passively aggressively correct James' whitespace issues

--- src/debounce.py
+++ src/debounce.py
@@ -6,21 +6,19 @@ def add_channels(*args):
     global channels
     for arg in args:
         channel.append(arg)
-    
+
 def serv_player1(channel):
     print('player1 served')
-    
+
 def serv_player2(channel):
     print('player2 served')
-    
+
 def powerup_player1(channel):
     print('player1 used a powerup')
-    
+
 def powerup_player2(channel):
     print('player2 used a powerup')
-    
-    
-    
+
 def setup():
     for ch in range(len(channel)):
         GPIO.setup(channel[ch], GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -31,7 +29,3 @@ def setup():
             GPIO.add_event_callback(channel[ch], powerup_player1, bouncetime = 200) 
         elif ch ==3:
             GPIO.add_event_callback(channel[ch], powerup_player2)
-
-
-        
-    
