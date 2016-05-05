d9e8bb716255b2a64742c733cb91c2b4fa39e09e
Peter Ryan Smith <ps1011@york.ac.uk>
2016-05-05 01:15:05 +0100

    Disassociate the lifetime of a pattern with the pattern. This is handled exclusively by the sequencer

--- src/glow_seq.py
+++ src/glow_seq.py
@@ -14,9 +14,8 @@ class GlowPattern(object):
         expire_time: The point in time where the pattern is considered
             exhausted
     """
-    def __init__(self, expire_time):
-        self.expire_time = expire_time
-        self.light_function_association = {x: wave_gen.ConstantSignal(0) for x in py_glow.LED_LIST}
+    def __init__(self):
+        self.light_function_association = {led: wave_gen.ConstantSignal(0) for led in py_glow.LED_LIST}
 
     def sample(self, sample_time):
         return {x: self.light_function_association[x].sample(sample_time)
@@ -31,14 +30,14 @@ class GlowSequencer(object):
         self.pi_glow_obj = pi_glow_obj
         self.pattern_stack = []
 
-    def insert(self, pattern):
-        self.pattern_stack.append(pattern)
+    def insert(self, pattern, duration):
+        self.pattern_stack.append((pattern, duration + self.current_time))
 
     def sample(self, time_delta):
         self.current_time += time_delta
-        self.pattern_stack = filter(lambda x: x.expire_time > self.current_time, self.pattern_stack)
-        sample_buffer = {x: 0.0 for x in py_glow.LED_LIST}
-        for i in self.pattern_stack:
-            sample_buffer.update(i.sample(self.current_time))
+        self.pattern_stack = filter(lambda pattern: pattern[1] > self.current_time, self.pattern_stack)
+        sample_buffer = {led: 0.0 for led in py_glow.LED_LIST}
+        for pattern in self.pattern_stack:
+            sample_buffer.update(pattern[0].sample(self.current_time))
         for (led, value) in sample_buffer.items():
             self.pi_glow_obj.led(led, value)
